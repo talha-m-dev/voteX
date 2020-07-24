1e3ab3ee49caa14f3b8b2121ebdfdb3d52227176
Talha Manzoor <talha.5023269@talmeez.pk>
2020-07-24 16:10:39 +0500

    Added Date Component

--- Screens/Election_commission/Add_org.js
+++ Screens/Election_commission/Add_org.js
@@ -3,7 +3,8 @@ import {StyleSheet, Image, View, Text,TouchableOpacity} from 'react-native';
 import { Kaede } from 'react-native-textinput-effects';
 import ImagePicker from 'react-native-image-picker';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {Button} from 'react-native-elements';
+import {Button, Overlay} from 'react-native-elements';
+import DatePicker from 'react-native-date-picker'
 
 
 export default function Add_Org() {
@@ -32,6 +33,8 @@ export default function Add_Org() {
     });
     }
   var [fileUri, SetFileuri] = useState();
+  const [date, setDate] = useState(new Date());
+  const [overlay, setoverlay] = useState(false)
 
  
   return (
@@ -40,6 +43,20 @@ export default function Add_Org() {
     alignSelf: 'center',
     marginTop: 35,
     }}>
+      <Overlay isVisible={overlay} >
+      <View>
+      <DatePicker
+      date={date}
+      style={{height:140,width:240}}
+      onDateChange={setDate}
+      
+    />
+    <TouchableOpacity onPress={()=>setoverlay(false)}>
+      <Text>Close Me</Text>
+    </TouchableOpacity>
+      </View>
+    
+    </Overlay>
     <Image
     style={{ height: 100, width: 150, borderRadius: 2, }}
     source={fileUri ? { uri: fileUri } : require('./assets/dummy.png')}
@@ -65,7 +82,28 @@ export default function Add_Org() {
         inputPadding={16}
       />
     </View>
+    
     <View style={styles.btn_con}>
+    <Button
+          buttonStyle={styles.btn}
+          icon={<Icon name="check" size={25} color="black" />}
+          title=" Voting Starting Time  "
+          type="outline"
+          iconRight={true}
+          onPress={() => {
+            setoverlay(true);
+          }}
+        />
+    <Button
+          buttonStyle={styles.btn}
+          icon={<Icon name="check" size={25} color="black" />}
+          title=" Voting Ending Time  "
+          type="outline"
+          iconRight={true}
+          onPress={() => {
+            setoverlay(true);
+          }}
+        />
     <Button
           buttonStyle={styles.btn}
           icon={<Icon name="check" size={25} color="black" />}
@@ -107,7 +145,7 @@ const styles = StyleSheet.create({
       justifyContent: 'space-around',
     },
     btn: {
-      marginTop:200,
+      marginTop:2,
       paddingVertical: 25,
       
       backgroundColor:'#fff',
--- android/app/src/main/java/com/votex/MainApplication.java
+++ android/app/src/main/java/com/votex/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.henninghall.date_picker.DatePickerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
